2d1881136eb359fad8373327744b473ea2bda6b8
Caleb Parrra <mccalebyt@gmail.com>
2022-08-17 15:35:19 -0500

    server w singleton patron

--- src/expressExample/index.js
+++ src/expressExample/index.js
@@ -1,33 +1,3 @@
-const express = require('express')
-const morgan = require('morgan')
+const {server} = require('./network')
 
-const { routes: { userRouter } } = require('./network')
-const { mongo: { dbConnection } } = require('./database')
-const response = require('./network/routes/response')
-
-const { connect } = dbConnection()
-const app = express()
-const PORT = process.env.PORT
-
-
-const main = async () => {
-  app.use(express.json())
-  app.use(morgan('dev'))
-  app.use(userRouter)
-
-  app.use((req, res) => {
-    response({
-      message: 'This route does not exists',
-      res,
-      status: 404
-    })
-  })
-
-  await connect()
-
-  app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
-  })
-}
-
-main()
+server.start()
