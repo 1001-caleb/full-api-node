6fbb2e2901fe6b75ba2ba9142f40174023efafad
Caleb Parrra <mccalebyt@gmail.com>
2022-08-31 14:14:49 -0500

    jwt

--- src/expressExample/network/routes/user.js
+++ src/expressExample/network/routes/user.js
@@ -1,12 +1,8 @@
 const { Router } = require('express')
+const jwt = require('jsonwebtoken')
 
 const {
-  user: {
-    StoreuserSchema,
-    userIdSchema,
-    UpdateUserSchema,
-    userLoginSchema
-  }
+  user: { StoreuserSchema, userIdSchema, UpdateUserSchema, userLoginSchema }
 } = require('../../schemas')
 const { validatorCompiler } = require('./utils')
 const { UserService } = require('../../services')
@@ -14,15 +10,16 @@ const response = require('./response')
 
 const userRouter = Router()
 
-userRouter.route('/user')
-  .get(async (req, res, next) => {
+userRouter.route('/user').get(
+  async (req, res, next) => {
     try {
       const userService = new UserService()
       response({ error: false, message: await userService.getAllUsers(), res, status: 200 })
     } catch (error) {
       next(error)
     }
-  })
+  }
+)
 
 userRouter.route('/user/signup').post(
   validatorCompiler(StoreuserSchema, 'body'),
@@ -49,18 +46,35 @@ userRouter.route('/user/signup').post(
   }
 )
 
-userRouter.route('/user/login')
-  .post(validatorCompiler(userLoginSchema, 'body'),
-    async (req, res, next) => {
-      try {
-        const { body: { email, password } } = req
+userRouter.route('/user/login').post(
+  validatorCompiler(userLoginSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const {
+        body: { email, password }
+      } = req
 
-        response({ error: false, message: await new UserService({ email, password }).login(), res, status: 200 })
-      } catch (error) {
-        next(error)
-      }
+      const payload = { email, password }
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '2min'
+      })
+
+      console.log('token', token)
+
+      response({
+        error: false,
+        message: await new UserService({
+          email,
+          password
+        }).login(),
+        res,
+        status: 200
+      })
+    } catch (error) {
+      next(error)
     }
-  )
+  }
+)
 
 userRouter.route('/user/:id')
   .get(
@@ -74,7 +88,8 @@ userRouter.route('/user/:id')
       } catch (error) {
         next(error)
       }
-    })
+    }
+  )
 
   .delete(
     validatorCompiler(userIdSchema, 'params'),
@@ -87,7 +102,8 @@ userRouter.route('/user/:id')
       } catch (error) {
         next(error)
       }
-    })
+    }
+  )
 
   .patch(
     validatorCompiler(userIdSchema, 'params'),
@@ -100,5 +116,6 @@ userRouter.route('/user/:id')
       } catch (error) {
         next(error)
       }
-    })
+    }
+  )
 module.exports = userRouter
